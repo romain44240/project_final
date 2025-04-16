b0a091869ddf3f0ca66086d845b5fcef5b9e39bf
yannisVoynnet <yvoynnet@gmail.com>
2025-04-16 10:14:09 +0200

    Version SpringBoot correcte

---
+++ projet-final/src/main/java/projet/ProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFinalApplication.class, args);
+	}
+
+}
---
+++ projet-final/src/test/java/projet/ProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
