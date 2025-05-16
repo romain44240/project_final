fc711cd3c95b2735f5da1f58fd9de25733b47292
Leenprle <leenapermallee@gmail.com>
2025-05-16 14:36:08 +0200

    Carte des produits

--- projet-angular/src/app/carte/carte.component.ts
+++ projet-angular/src/app/carte/carte.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environment';
+import { AdminService } from '../service/admin.service';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { combineLatest, Observable, of } from 'rxjs';
+import { Consommable, Jeu } from '../models';
 
 @Component({
   selector: 'app-carte',
@@ -6,6 +12,23 @@ import { Component } from '@angular/core';
   templateUrl: './carte.component.html',
   styleUrl: './carte.component.css'
 })
-export class CarteComponent {
+export class CarteComponent implements OnInit{
+private API_URL: string = `${environment.API_URL}`;
 
+  constructor(private service: AdminService, private router: Router){}
+
+  public products : Observable<{consos: Consommable[]; }> = of({consos: [] });
+
+  public conso : Observable<Consommable[]> | undefined;
+ 
+
+
+  ngOnInit(): void {
+  
+    this.conso = this.service.getAllConsos();
+
+    this.products = combineLatest({
+      consos: this.conso
+    });
+  }
 }
